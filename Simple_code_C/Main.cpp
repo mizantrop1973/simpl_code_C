2b3ab9fb0bfe8de4171abc48d6a0ea00bfa05d46
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-01-28 23:32:22 +0300

    Рандомный массив с уникальными числами

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -1,8 +1,28 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
 
-int main()
+void main()
 {
-	cout << "Hello World!" << endl;
+	srand(time(NULL));
+	const int SIZE = 10;
+	int arr[SIZE];
+
+	for (int i = 0; i < SIZE; ++i)
+	{
+		arr[i] = rand() % 20;
+		for (int j=i-1; j >= 0; --j)
+		{
+			if (arr[i] == arr[j])
+			{
+				arr[i] = rand() % 20;
+				j = i;
+			}
+		}
+	}
+
+
+	for (int i = 0; i < SIZE; ++i)
+		cout << arr[i]<<endl;
 }
