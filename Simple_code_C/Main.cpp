718c5c9dbd4b1db1698611b48e4e9b19b01f7d00
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-02-11 21:20:53 +0300

    Пишем класс String продолжение

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -9,266 +9,86 @@ using namespace std;
 class String
 {
 public:
+	// КОНСТРУКТОР ПО УМОЛЧАНИЮ
+	
 	String()
 	{
-		size = 0;
-		char* str = new char[size+1];
-		str[0] = '\0';
-	}
-	~String()
-	{
-		delete[] str;
+		str = nullptr;
 	}
 
-	String(const String& other)                   // Конструктор копирования
+
+	// КОНСТРУКТОР ПО ПАРАМЕТРУ
+
+	String(const char* str)
+
 	{
-		this->size = other.size;
-		this->str = new char [other.size +1];
-		for (int i = 0; i < other.size; ++i)
+		for (int i = 0; str[i] != '\0'; ++i)
+			length = i + 1;
+		this->str = new char[length + 1];
+		for (int i = 0; i < length; ++i)
+			this->str[i] = str[i];
+		this->str[length] = '\0';
+	}
+	
+	// КОНСТРУКТОР КОПИРОВЫАНИЯ
+
+	String(const String& other)                  
+	{
+		this->length = other.length;
+		this->str = new char [length +1];
+		for (int i = 0; i < length; ++i)
 		{
 			this->str[i] = other.str[i];
 		}
-		str[other.size] = '\0';
+		str[length] = '\0';
 	}                
 
-	String& operator = (const String& other)              // Конструктор присваивания
+	// ПЕРЕГРУЗКА ОПЕРАТОРА ПРИСВАИВАНИЯ
+
+	String& operator = (const String& other)              
 	{
-		this->size = other.size;
 		if (this->str != nullptr)            //обычно не требуется, так как объект создан уже
 			delete[] this->str;
-		this->str = new char[other.size+1];
-		for (int i = 0; i < other.size; ++i)
-		{
+		for (int i = 0; other.str[i] != '\0'; ++i)
+			length = i + 1;
+		this->str = new char[length + 1];
+		for (int i = 0; i < length; ++i)
 			this->str[i] = other.str[i];
-		}
-		str[other.size] = '\0';
+		this->str[length] = '\0';
 
 		return *this;
 	}
 
-	void operator << (const String& other)              // Конструктор присваивания
-	{
-		for (int i = 0; i < other.size; ++i)
-			cout << other.str[i];
-	}
+	// ПЕРЕГРУЗКА ОПЕРАТОРА КОНТАКЦЕНАЦИИ(СКЛЕИВАНИЯ)
 
-	char* GetStr()
-	{
-		return str;
-	}
+	String operator + (const String& other)
 
-	int GetSize()
 	{
-		return size;
-	}
-
-	String& operator = (const char* other)              // Конструктор присваивания
-	{
-		for (int i = 0; other[i] != '\0'; ++i)
-			size = i + 1;
-		this->size = size;
-		if (this->str != nullptr)            //обычно не требуется, так как объект создан уже
-			delete[] this->str;
-		this->str = new char[size + 1];
-		for (int i = 0; i < size; ++i)
-		{
-			this->str[i] = other[i];
-		}
-		str[size] = '\0';
-
-		return *this;
+		String str1;
+		str1.str = new char[this->length + other.length + 1];
+		for (int i = 0; i < this->length + other.length; ++i)
+			i < this->length ? str1.str[i] = this->str[i] : str1.str[i] = other.str[i-this->length];
+		str1.length = this->length + other.length;
+		str1.str[str1.length] = '\0';
+		return str1;
 	}
 
 	void Print()
 	{
-		for (int i = 0; i < size; ++i)
-			cout << str[i];
-		cout << endl << endl;
+		cout << str << endl;
 	}
+
+	~String()
+	{
+		delete[] this->str;
+	}
+
 private:
 	char* str;
-	int size;
+	int length;
+	
 };
 
-
-
-
-class Human
-{
-
-public:
-	int age;  //поля класса
-	string name;
-	int weight;
-
-	void Print()
-	{
-		cout <<"Имя "<< name << " \n Возраст" << age << " \n Вес " << weight << endl << endl;
-	}
-};
-
-class Point
-{
-public:
-
-	Point()
-	{
-		x = 0;
-		y = 0;
-		cout << this << " constructor" << endl;
-	}
-
-	Point(int valueX, int valueY) // конструктор
-	{
-		x = valueX;
-		y = valueY;
-		cout << this << " constructor" << endl;
-	}
-
-	bool operator ==(const Point& other)                               // перегрузка оператора сравнения (равенства)
-	{
-		return this->x == other.x && this->y == other.y;
-	}
-
-	bool operator !=(const Point& other)                               // перегрузка оператора сравнения (неравенства)
-	{
-		return !(this->x == other.x && this->y == other.y);
-	}
-
-	int GetX()
-	{
-		return x;
-	}
-
-	void SetX(int valueX)
-	{
-		x = valueX;
-	}
-
-	int GetY()
-	{
-		return y;
-	}
-
-	void SetY(int y)
-	{
-		this->y = y;
-	}
-
-	void Print()
-	{
-		cout << "X = " << x << "\t Y = " << y << endl << endl;
-	}
-
-private:
-	int x;
-	int y;
-};
-
-class CoffeeGrinder
-{
-private:
-	bool CheckVoltage()
-	{
-		return true;
-	}
-
-
-public:
-	void Start()
-	{
-		if (CheckVoltage())
-			cout << " VjuHHHHH" << endl;
-		else
-			cout << " Beep Beep" << endl;
-		
-
-	}
-};
-
-class MyClass
-{
-public:	
-	int* data;
-	//int* data;
-
-
-	MyClass(int size)                                 
-	{
-		this->size = size;
-		this->data = new int[size];
-		for (int i = 0; i < size; ++i)
-		{
-			data[i] = i;
-		}
-		cout << this <<" Вызвался конструктор" << endl;
-	}
-
-
-
-	MyClass(const MyClass &other)                   // Конструктор копирования
-	{
-		
-
-
-		this->size = other.size;
-		this->data = new int[other.size];
-		for (int i = 0; i < other.size; ++i)
-		{
-			this->data[i] = other.data[i];
-		}
-		cout << this << " вызвался конструктор копирования " << endl;
-	}                //конструктор копирования
-
-	MyClass& operator = (const MyClass& other)              // Конструктор присваивания
-	{
-		this->size = other.size;
-		if (this->data != nullptr)            //обычно не требуется, так как объект создан уже
-			delete[] this->data;
-		this->data = new int[other.size];
-		for (int i = 0; i < other.size; ++i)
-		{
-			this->data[i] = other.data[i];
-		}
-		cout << this << " вызвался конструктор присваивания" << endl;
-
-		return *this;
-	}
-
-
-
-	~MyClass()										// деструктор (только один)         
-	{
-		
-		delete[] data;
-		cout <<  this << " Вызвался деструткор" << endl;
-	}
-
-private:
-	int size;
-
-
-};
-
-void Foo ()
-{
-	cout << "Foo начало" << endl;
-	MyClass a(1);
-	cout << "Foo конец" << endl;
-
-}
-
-void Foo2(MyClass value)  //по значению
-{
-	cout << "Вызвалась функция Foo" << endl;
-}
-
-MyClass Foo3()  //по значению
-{
-	MyClass temp(2);
-	cout << "Вызвалась функция Foo3" << endl;
-	return temp;
-}
 
 
 
@@ -277,59 +97,20 @@ int main(int argc, char* argv[])
 
 {
 	setlocale(LC_ALL, "ru");
-	/*Human firsthuman;
-	firsthuman.age = 30;
-	firsthuman.name = "Ivanov Ivan Ivanovich";
-	firsthuman.weight = 80;
-
-	firsthuman.Print();*/
-
-	/*Point a;
-	a.SetX(5);
-	a.SetY(11);
-	a.Print();
-	cout << a.GetX() << endl;
-	a.SetY(5);
-	a.Print();
-
-	Point b(7, 66);
-	b.Print();*/
 	
-	/*Point a(1, 9);
-	Point b(3, 9);
+	
+	String a("jbivuiefbujen uenb ubmbenboe");
+	
+	String b = "hbhdfb b uiebieb";
 
-	bool result = a != b;
-
-
-	cout << result << endl;*/
-
-	/*CoffeeGrinder a;
-	a.Start();
-
-	MyClass a(1);
-
-	MyClass b(2);
-
-	Foo();*/
-
-
-	/*MyClass a(10);
-	MyClass b(2);
-	MyClass c(7);
-	c = a = b;*/
-
-	//Foo2(a);
-
-	String a;
-	a = "hhbbl";
-
-	String b = a;
+	String result;
+	result = a + b;
 	
 
 	
 	a.Print();
 	b.Print();
-	
+	result.Print();
 	
 
 	return 0;
