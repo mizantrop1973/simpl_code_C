04a83e24fbd1ee2aec28f69b3cb215b3de01679f
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-02-14 04:21:15 +0300

    Переменные static. Пример генерации ID самостоятельно

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -11,39 +11,49 @@ class Point;
 
 class Human
 {
+	static int Count;
 public:
-	void paintPoint(const Point&  point);       // в C++ обязательно выносить реализацию дружественных другим классам методов вне класса
-	void ClearPoint(Point& point);
+	Human(int age, string name, int weight)
+	{
+		this->age = age;
+		this->name = name;
+		this->weight = weight;
+		++Count;
+		id = Count;		 
+	}
 
-	
+	void Print()
+	{
+		cout << "ID " << this->id << "\t" << "Возраст: " << this->age << "\t" << "Имя: " << this->name << "\t" << "Вес: " << this->weight << endl << endl;
+	}
 	
 private:
 	int age;  //поля класса
 	string name;
 	int weight;
-
-	void Print()
-	{
-		cout <<"Имя "<< name << " \n Возраст" << age << " \n Вес " << weight << endl << endl;
-	}
+	int id;
 };
+
+int Human::Count = 1000000;
 
 class Point
 {
-	friend Human;
 public:
+	static int Count;
 
 	Point()
 	{
 		x = 0;
 		y = 0;
 		cout << this << " constructor" << endl;
+		++Count;
 	}
 
 	Point(int valueX, int valueY) // конструктор
 	{
 		x = valueX;
 		y = valueY;
+		++Count;
 		cout << this << " constructor" << endl;
 	}
 
@@ -103,162 +113,7 @@ private:
 	int y;
 };
 
-class CoffeeGrinder
-{
-private:
-	bool CheckVoltage()
-	{
-		return true;
-	}
-
-
-public:
-	void Start()
-	{
-		if (CheckVoltage())
-			cout << " VjuHHHHH" << endl;
-		else
-			cout << " Beep Beep" << endl;
-		
-
-	}
-};
-
-class MyClass
-{
-public:	
-	int* data;
-	//int* data;
-	     
-
-	MyClass(int size)                                 
-	{
-		this->size = size;
-		this->data = new int[size];
-		for (int i = 0; i < size; ++i)
-		{
-			data[i] = i;
-		}
-		cout << this <<" Вызвался конструктор" << endl;
-	}
-
-
-
-	MyClass(const MyClass &other)                   // Конструктор копирования
-	{
-		
-
-
-		this->size = other.size;
-		this->data = new int[other.size];
-		for (int i = 0; i < other.size; ++i)
-		{
-			this->data[i] = other.data[i];
-		}
-		cout << this << " вызвался конструктор копирования " << endl;
-	}                //конструктор копирования
-
-	MyClass& operator = (const MyClass& other)              // Конструктор присваивания
-	{
-		this->size = other.size;
-		if (this->data != nullptr)            //обычно не требуется, так как объект создан уже
-			delete[] this->data;
-		this->data = new int[other.size];
-		for (int i = 0; i < other.size; ++i)
-		{
-			this->data[i] = other.data[i];
-		}
-		cout << this << " вызвался конструктор присваивания" << endl;
-
-		return *this;
-	}
-
-
-
-	~MyClass()										// деструктор (только один)         
-	{
-		
-		delete[] data;
-		cout <<  this << " Вызвался деструткор" << endl;
-	}
-
-private:
-	int size;
-
-
-};
-
-void Foo ()
-{
-	cout << "Foo начало" << endl;
-	MyClass a(1);
-	cout << "Foo конец" << endl;
-
-}
-
-void Foo2(MyClass value)  //по значению
-{
-	cout << "Вызвалась функция Foo" << endl;
-}
-
-MyClass Foo3()  //по значению
-{
-	MyClass temp(2);
-	cout << "Вызвалась функция Foo3" << endl;
-	return temp;
-}
-
-class TestClass                     //перегрузка оператора индексирования []
-{
-public:
-	int& operator [](int index)
-	{
-		return arr[index];
-	}
-	
-
-private:
-	int arr[5]{5,44,55,77,99};
-};
-
-void ChangeX(Point& value)  // дружественная функция может принимать аргументы из разных классов, где она задекларирована как 
-{							//  дружественная
-	value.x = -1;
-}
-
-class Cls                                  //вынос определения методов из класса
-{
-public:
-	Cls();
-	~Cls();
-
-	void PrintMessage();
-
-private:
-
-};
-
-Cls::Cls()
-{
-}
-
-Cls::~Cls()
-{
-}
-
-void Cls::PrintMessage()
-{
-	cout << "Hello" << endl;
-}
-
-void Human::paintPoint(const Point&  point)       //нарушается инкапсуляции, чтобы не нарушалась например надо передавать const ccылку
-{
-	
-	
-	cout << "X = " << point.x <<"\t"<<"Y = " << point.y << endl;
-	
-}
-
+int Point::Count = 0;                           /// инициализация статичной переменной должна быть вне класса!!!!
 
 
 int main(int argc, char* argv[])
@@ -266,76 +121,31 @@ int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "ru");
 
-	// ОПЕРАЦИИ С Human
-
-	/*Human firsthuman;
-	firsthuman.age = 30;
-	firsthuman.name = "Ivanov Ivan Ivanovich";
-	firsthuman.weight = 80;
-
-	firsthuman.Print();*/
-
-	//ОПЕРАЦИИ С Point
-
-	/*Point a;
-	a.SetX(5);
-	a.SetY(11);
-	a.Print();
-	Point  d = ++a;
-	d.Print();
-	a.Print();
-	d = a++;
-	d.Print();
-	a.Print();
-
-	cout << a.GetX() << endl;
-	a.SetY(5);
-	a.Print();*/
-
 	Point b(7, 66);
-
-	Human a;
+	Point a;
+	Point c(2, 3);
 	
-	a.paintPoint(b);
+	/// Point::Count = 3;                 неинициализированная переменная static дает ошибку
+
+	cout << b.Count << endl;  
+
+	//или
+
+	cout << Point::Count << endl;
+
+	a.Print();
 	b.Print();
-
-	
-	/*b.Print();
-
-	Point c = a + b;
-
 	c.Print();
 
-	ChangeX(a);     // дружественная ф-ция
-	a.Print();*/
+	Human d(25, "hbh", 78);
+	Human e(44, "okepw", 90);
+	Human f(34, "jvioerv", 65);
 
-	// ОПЕРАЦИИ С CoffeeGrinder
+	d.Print();
+	e.Print();
+	f.Print();
+		
+
 	
-	/*CoffeeGrinder a;
-	a.Start();*/
-
-	// ОПЕРАЦИИ С MyClass
-
-	/*MyClass a(1);
-
-	MyClass b(2);
-
-	Foo();
-
-	MyClass a(10);
-	MyClass b(2);
-	MyClass c(7);
-	c = a = b;
-
-	//Foo2(a);*/
-
-	// ОПЕРАЦИИ С Test
-	/*int arr[]{ 5,6,7,8,9 };
-	TestClass a;
-
-	cout << a[1];
-	return 0;*/
-
-	/*Cls a;
-	a.PrintMessage();*/
+	
 }
