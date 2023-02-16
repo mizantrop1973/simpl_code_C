4c9ec9a7770817db71ef70457443e65130ab346e
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-02-16 21:30:26 +0300

    Делегирующие конструкторы

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -6,180 +6,51 @@
 #include<string>
 using namespace std;
 								
-											/// ЧИСТО ВИРТУАЛЬНЫЙ ДЕСТРУКТОР
+											/// ДЕЛЕГИРУЮЩИЙ КОНСТРУКТОР
 
-class Weapon
+
+
+class Human
 {
 public:
-	Weapon();
-	virtual ~Weapon() = 0;					/// чисто виртуальный деструктор 
+	Human(string name);
+	Human(string name, int age);
+	Human(string name, int age, int weight);
+	~Human();
 
-
-	virtual void Shoot() = 0;            /// чисто виртуальная функция , если есть хоть одна чисто виртуальная ф-ция, то класс абстрактный
-										/// тут так же могут быть определенные методы, но класс уже абстрактный
-	void Foo()
-	{
-		cout << "Foo" << endl;
-	}
-
+	string name;
+	int age;
+	int weight;
 private:
 
 };
 
-Weapon::Weapon() 
+Human::Human(string name)
 {
-	
-};					
-
-
-Weapon::~Weapon() {};           /// в чисто виртуальном деструкторе обязательно выносить определение деструктора в данном синтаксисе
-
-
-class Gun:public Weapon
-{
-public:
-	Gun();
-	~Gun() override;			/// переопределение виртуального деструктора
-	void Shoot() override               
-	{									
-		cout << "BANG!!" << endl;		
-	}
-private:
-
-};
-
-Gun::Gun()
-{
-	cout << "выделена динамическая память Gun" << endl;
+	this->name = name;
 }
 
-
-Gun::~Gun()
+Human::Human(string name, int age) : Human(name)
 {
-	cout << "освобождена динамическая память Gun" << endl;
+	this->age = age;
 }
 
-//class SubMachineGun :public Gun
-//{
-//public:
-//	SubMachineGun();
-//	~SubMachineGun();
-//
-//	void Shoot() override                      ///override не обязателен, но он контролирует идентичность сигнатуры метода Shoot
-//	{
-//		cout << "BANG!!  BANG!!  BANG!!" << endl;
-//	}
-//
-//private:
-//
-//};
-//
-//SubMachineGun::SubMachineGun()
-//{
-//	cout << "выделена динамическая память SubMachineGun" << endl;
-//}
-//
-//SubMachineGun::~SubMachineGun()
-//{
-//	cout << "освобождена динамическая память SubMachineGun" << endl;
-//}
-//
-//class Bazooka:public Weapon
-//{
-//public:
-//	Bazooka();
-//	~Bazooka();
-//	void Shoot() override                      ///override не обязателен, но он контролирует идентичность сигнатуры метода Shoot
-//	{
-//		cout << "BARABOOOM!!!" << endl;
-//	}
-//
-//private:
-//
-//};
-//
-//Bazooka::Bazooka()
-//{
-//	cout << "выделена динамическая память Bazooka" << endl;
-//}
-//
-//Bazooka::~Bazooka()
-//{
-//	cout << "освобождена динамическая память Bazooka" << endl;
-//}
-//
-//class Knife: public Weapon
-//{
-//public:
-//	Knife();
-//	~Knife();
-//	void Shoot() override                      ///в наследниках абстрактного класса виртуальный метод (чисто) должен быть обязательно определен
-//	{
-//		cout << "Vjuuuhhh" << endl;
-//	}
-//private:
-//
-//};
-//
-//Knife::Knife()
-//{
-//	cout << "выделена динамическая память Knife" << endl;
-//}
-//
-//Knife::~Knife()
-//{
-//	cout << "освобождена динамическая память Knife" << endl;
-//}
-
-class Player
+Human::Human(string name, int age, int weight) :Human(name, age)
 {
-public:
-	Player();
-	~Player();
-
-	void Shoot(Weapon* weapon)
-	{
-		weapon->Shoot();
-	}
-
-
-private:
-
-};
-
-Player::Player()
-{
+	this->weight = weight;
 }
 
-Player::~Player()
+Human::~Human()
 {
 }
-
 
 int main(int argc, char* argv[])
 
 {
 	setlocale(LC_ALL, "ru");
 
-	/*Gun gun;
-	SubMachineGun machinegun;
-	Bazooka bazooka;
-	Knife knife;
-
-	Player player;
-
-	player.Shoot(&machinegun);
-	player.Shoot(&gun);
-	player.Shoot(&bazooka);
-	player.Shoot(&knife);
+	Human human("Иванов", 25, 78);
 	
-	knife.Foo();*/
-
-	Weapon* gunptr = new Gun;
-	delete gunptr;
-
-
-	///Weapon weapon;   /// так как метод стрелять закомментирован, у нас только виртуальный деструктор
-	return 0;		    /// он делает класс абстрактным
+	return 0;		    
 
 }
