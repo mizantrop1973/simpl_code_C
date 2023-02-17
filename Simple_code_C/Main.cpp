69dc8aa754583a4ae1b3cca59b2921527da54db8
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-02-17 21:17:13 +0300

    Работа с файлами. Запись в файл

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -1,21 +1,55 @@
-#include<iostream>
-#include<ctime>
-#include <math.h>
-#include<algorithm>
-#include<locale.h>
+#include<iostream>							/// ввод-вывод
+#include<ctime>								/// время
+#include <math.h>							/// математические ф-ции
+#include<algorithm>							/// библиотека ф-ций
+#include<locale.h>							/// язык
+#include<fstream>							/// поток в файлы
 using namespace std;
 
 
+											///Работа с файлами
 
-
-void main(int argc, char* argv[])
+int  main()
 
 {
 	setlocale(LC_ALL, "ru");
-	for (int i = 0; i < argc; ++i)
-		cout << argv[i] << endl;  //выдает хотя бы один параметр - адрес экзешника в папке с проектом
-								  //если скопировать адрес экзешника в командную строку, можно через пробел можно передать массу параметров любых (набираем символы)
+	string path = "myFile.txt";
+	//cin >> path;
+	ofstream fout;
+	//fout.open("myFile.txt");   /// в таком виде файл создается в папке с проектом, или полный адрес иначе
+	/*fout.open(path);			///    затирает все прежнее
 
-	system("pause");
+	if (!fout.is_open())		/// fout.is_open()  возвращает   bool , 
+	{
+		cout << "Error of file's opening" << endl;
+	}
+	else 
+	{
+		fout << "These are our datas" << endl;
+	}
+
+	fout.close();				/// закрываем файл*/
+
+	fout.open(path, ofstream::app);           /// добавляем, не затираем
+
+	if (!fout.is_open())		/// fout.is_open()  возвращает   bool , 
+	{
+		cout << "Error of file's opening" << endl;
+	}
+	else
+	{
+		for (size_t i = 0; i < 7; ++i)
+		{
+			cout << "Enter a number:" << endl;
+			int a;
+			cin >> a;
+			//fout << endl<< "NEW datas" << endl;
+			fout << a << endl;
+		}
+	}
+
+	fout.close();				/// закрываем файл
+
+	return 0;
 
 }
