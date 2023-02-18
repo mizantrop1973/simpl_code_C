620df2a32e9b610ed1b3eaf15fff422e1ad854ab
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-02-18 17:57:51 +0300

    Запись / чтение объектов класса

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -8,7 +8,55 @@
 using namespace std;
 
 
-											///Работа с файлами
+											///Чтение и запись объекта класса из файла/в файл
+
+class Point
+{
+public:
+
+	Point()
+	{
+		x = y = z = 0;
+	}
+
+	Point(int x, int y, int z )// конструктор
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+
+	int GetX()
+	{
+		return x;
+	}
+
+	void SetX(int valueX)
+	{
+		x = valueX;
+	}
+
+	int GetY()
+	{
+		return y;
+	}
+
+	void SetY(int y)
+	{
+		this->y = y;
+	}
+
+	void Print()
+	{
+		cout << "\t X = " << x << "\t Y = " << y << "\t Z = " << z << endl << endl;
+	}
+
+private:
+	int x;
+	int y;
+	int z;
+};
+
 
 int  main()
 
@@ -16,20 +64,20 @@ int  main()
 	setlocale(LC_ALL, "ru");
 	string path = "myFile.txt";
 	//cin >> path;
-	ofstream fout;
+	//ofstream fout;
 	//fout.open("myFile.txt");   /// в таком виде файл создается в папке с проектом, или полный адрес иначе
-	/*fout.open(path);			///    затирает все прежнее
+	///*fout.open(path);			///    затирает все прежнее
 
-	if (!fout.is_open())		/// fout.is_open()  возвращает   bool , 
-	{
-		cout << "Error of file's opening" << endl;
-	}
-	else 
-	{
-		fout << "These are our datas" << endl;
-	}
+	//if (!fout.is_open())		/// fout.is_open()  возвращает   bool , 
+	//{
+	//	cout << "Error of file's opening" << endl;
+	//}
+	//else 
+	//{
+	//	fout << "These are our datas" << endl;
+	//}
 
-	fout.close();				/// закрываем файл*/
+	//fout.close();				/// закрываем файл*/
 
 	//fout.open(path, ofstream::app);           /// добавляем, не затираем
 
@@ -51,8 +99,55 @@ int  main()
 
 	//fout.close();				/// закрываем файл
 
-	ifstream fin;
+	//ifstream fin;
 
+	//fin.open(path);
+	//if (!fin.is_open())		/// fout.is_open()  возвращает   bool , 
+	//{
+	//	cout << "Error of file's opening" << endl;
+	//}
+	//else
+	//{
+	//	cout << "File is open" << endl;
+	//	char str1[50]{};
+	//	char ch;
+	//	string str;
+	//	//while (fin.get(ch))						/// посимвольно считываем, в т.ч. переносы строк (неявные)
+	//	while(!fin.eof())							/// eof считывает строки  ДО окончания файла
+	//	{
+	//		//cout << ch;
+	//		str = "";								/// исправит ошибку двойного вывода последней строки
+	//		//fin >> str;								/// ВНИМАНИЕ. Метод считывая строку ДО ПЕРВОГО ПРОБЕЛА! 
+	//		getline(fin, str/*,";"*/);				/// считывает строку ДО "сепаратора" - третий, необязательный аргумент, по умолчанию -  \n
+	//		//fin.getline(str1, 50);				/// неудобный метод класса ifstream, ограничивает количество символов, и str1 должен быть массивом
+	//		
+	//		
+	//		cout << str << endl;					/// считывает файл до конца, при этом если есть в конце перенос строки - 
+	//												/// отработает еще раз с последним данными
+	//	}
+	//}
+	//
+	//fin.close();
+
+	/*Point point(0, 0, 0);
+	point.Print();
+
+	ofstream fout;
+	fout.open(path, ofstream::app);
+	
+	if (!fout.is_open())		/// fout.is_open()  возвращает   bool , 
+	{
+		cout << "\t Error of file's opening" << endl;
+	}
+	else
+	{
+		cout << "\t File is open" << endl;
+		fout.write((char*)&point, sizeof(Point));
+	}
+
+	fout.close();				/// закрываем файл*/
+
+	ifstream fin;
 	fin.open(path);
 	if (!fin.is_open())		/// fout.is_open()  возвращает   bool , 
 	{
@@ -61,27 +156,15 @@ int  main()
 	else
 	{
 		cout << "File is open" << endl;
-		char str1[50]{};
-		char ch;
-		string str;
-		//while (fin.get(ch))						/// посимвольно считываем, в т.ч. переносы строк (неявные)
-		while(!fin.eof())							/// eof считывает строки  ДО окончания файла
+		Point point1;
+		while (fin.read((char*)&point1, sizeof(Point)))       /// считывает один объект
 		{
-			//cout << ch;
-			str = "";								/// исправит ошибку двойного вывода последней строки
-			//fin >> str;								/// ВНИМАНИЕ. Метод считывая строку ДО ПЕРВОГО ПРОБЕЛА! 
-			//getline(fin, str/*,";"*/);				/// считывает строку ДО "сепаратора" - третий, необязательный аргумент, по умолчанию -  \n
-			fin.getline(str1, 50);					/// неудобный метод класса ifstream, ограничивает количество символов, и str1 должен быть массивом
-			
-			
-			cout << str1<<endl;						/// считывает файл до конца, при этом если есть в конце перенос строки - 
-													/// отработает еще раз с последним данными
-
-			
+			point1.Print();
 		}
 	}
-	
 	fin.close();
+
+
 	return 0;
 
 }
