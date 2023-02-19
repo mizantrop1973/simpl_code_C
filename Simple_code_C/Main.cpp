313cff4986222c928f6b06002134509f31ea98e8
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-02-19 03:28:00 +0300

    Операторы  вывода - перегрузка.

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -3,7 +3,9 @@
 #include <math.h>
 #include<algorithm>
 #include<locale.h>
-#include<string>
+#include<fstream>
+#include<Windows.h>
+//#include<string>
 using namespace std;
 
 class String
@@ -13,7 +15,8 @@ public:
 
 	String()
 	{
-		str = nullptr;
+		this->str = nullptr;
+		this->length = 0;
 	}
 
 	// ÊÎÍÑÒĞÓÊÒÎĞ ÏÎ ÏÀĞÀÌÅÒĞÓ
@@ -28,6 +31,19 @@ public:
 			this->str[i] = str[i];
 		this->str[this->length] = '\0';
 	}
+
+	String(char* str)
+
+	{
+		for (int i = 0; str[i] != '\0'; ++i)
+			this->length = i + 1;
+		this->str = new char[this->length + 1];
+		for (int i = 0; i < this->length; ++i)
+			this->str[i] = str[i];
+		this->str[this->length] = '\0';
+	}
+
+
 
 	// ÊÎÍÑÒĞÓÊÒÎĞ ÊÎÏÈĞÎÂÛÀÍÈß
 
@@ -119,13 +135,8 @@ public:
 		return this->str[index];
 	}
 
-	// ÏÅĞÅÃĞÓÇÊÀ ÂÛÂÎÄÀ
-
-	ostream& operator << (ostream& os, const String& str)
-	{
-		return os;
-	}
-	
+	friend ostream& operator<<(ostream& os, const String& string);
+	friend istream& operator>>(istream& os, String& string);
 
 	void Print()
 	{
@@ -141,20 +152,54 @@ public:
 	{
 		delete[] this->str;
 	}
-
+	
 private:
 	char* str;
 	int length;
 };
 
+// ÏÅĞÅÃĞÓÇÊÀ ÂÛÂÎÄÀ                                        
+
+
+ostream& operator<<(ostream& os, const String& string)
+{
+	os << string.str << endl;
+	return os;
+}
+
+ /// ÏÅĞÅÃĞÓÇÊÀ ÂÂÎÄÀ
+
+istream& operator>>(istream& is, String & string)
+{
+	
+	int max;
+	cout << "Point the max of simbol's quantity" << endl;
+	cin >> max;
+	
+	if (string.str != nullptr)
+		delete[] string.str;
+	char c;
+	String temp(cin.get());
+	
+	for (int i = 0; cin.get(temp.str[i]); ++i)
+	{
+		temp.length= i + 1;
+		string.str[i] = temp.str[i];
+	
+	 
+	
+	
+
+	return is;
+}
 
 int main()
 
 {
 	setlocale(LC_ALL, "ru");
 
-
-	String a("Hello");
+	string path = "MyFile.txt";
+	String a;
 
 	String b = ", World!";
 
@@ -171,14 +216,34 @@ int main()
 
 
 
-	a.Print();
+	//a.Print();
 	b.Print();
 	result.Print();
 
-	cout << a[0] << endl;
+	/*cout << a[0] << endl;
 	a[3] ='z';
-	a.Print();
-	a.operator<<();
+	a.Print();*/
+
+
+
+	
+	/*fstream fs;
+	fs.open(path, fstream::in | fstream::out | fstream::app);              ///   "|" - áèòîâîå  "èëè"
+
+	if (!fs.is_open())		/// fout.is_open()  âîçâğàùàåò   bool , 
+	{
+		cout << "Error of file's opening" << endl;
+	}
+	else
+	{
+		cout << "File is open" << endl;
+		fs << a << "\n";
+		
+	}
+	fs.close();*/
+
+	cin>>b;
+	
 	//cout << a;
 	return 0;
 
