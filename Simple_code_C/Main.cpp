55c44d4f1c70daeba54c544c0b7f9148b91dcd18
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-02-25 23:20:23 +0300

    Умные указатели НАЧАЛО

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -9,146 +9,51 @@
 using namespace std;
 
 
-															/// СПЕЦИАЛИЗАЦИЯ ШАБЛОНОВ КЛАССА
-															
-								                         
-
-//class Point
-//{
-//public:
-//	Point();
-//	Point(int x, int y, int z);
-//	~Point();
-//
-//private:
-//
-//	int x;
-//	int y;
-//	int z;
-//};
-//
-//
-//Point::Point()
-//{
-//	 x = y = z = 0;
-//}
-//
-//Point::Point(int x, int y, int z)
-//{
-//	this->x = x;
-//	this->y = y;
-//	this->z = z;
-//}
-//
-//Point::~Point()
-//{
-//}
-//
-//
-//template<typename/*или class*/ T1, typename T2>
-//class MyClass
-//{
-//public:
-//	MyClass(T1 value1, T2 value2);
-//	~MyClass();
-//
-//
-//	void DataTypeSize()
-//	{
-//		cout << sizeof(value1) << endl;
-//		cout << sizeof(value2) << endl;
-//	}
-//
-//
-//protected:
-//	T1 value1;
-//	T2 value2;
-//
-//	private:
-//
-//};
-//template <typename T1, typename T2>
-//MyClass<T1, T2>::MyClass(T1 value1, T2 value2)
-//{
-//	this->value1 = value1;
-//	this->value2 = value2;
-//}
-//template <typename T1, typename T2>
-//MyClass<T1, T2>::~MyClass()
-//{
-//}
-//
-//
-//template <typename T1, typename T2>
-//class TypeInfo : public MyClass<T1, T2>
-//{
-//public:
-//	TypeInfo(T1 value1, T2 value2) ;
-//	~TypeInfo();
-//
-//	void ShowTypeName()
-//	{
-//		cout << "Typename T1 is " << typeid(this->value1).name() << endl;
-//		cout << "Typename T2 is " << typeid(this->value2).name() << endl;
-//		
-//	}
-//
-//private:
-//
-//};
-//
-//template <typename T1, typename T2>
-//TypeInfo<T1, T2>::TypeInfo(T1 value1, T2 value2):MyClass<T1, T2>(value1, value2)
-//{
-//
-//}
-//
-//template <typename T1, typename T2>
-//TypeInfo<T1, T2>::~TypeInfo()
-//{
-//}
-template <typename T>
-class Printer
+															/// Умные_указатели_Smart_pointers
+template<class T>															
+class SmartPointer
 {
 public:
-	Printer();
-	~Printer();
-	void Print(T value)
+	SmartPointer(T* ptr);
+	~SmartPointer();
+
+	T& operator *()
 	{
-		cout << value << endl;
+		return *ptr;
 	}
+
 
 private:
-
+	T* ptr;
 };
-template <typename T>
-Printer<T>::Printer()
+
+template<class T>
+SmartPointer<T>::SmartPointer(T*ptr)
 {
+	this->ptr = ptr;
+	cout << "Constructor" << endl;
 }
-template <typename T>
-Printer<T>::~Printer()
+template<class T>
+SmartPointer<T>::~SmartPointer()
 {
+	delete ptr;
+	cout << "Destructor" << endl;
 }
 
-template<>
-class Printer<string>								/// Это и есть специализация. Ксли никакой реализации нет, то это запрет на string
-{
-public:
-	void Print(string value)
-	{
-		cout << "___"<<value << "___"<<endl;
-	}
 
-};
 
 int  main()
 
 {
 	setlocale(LC_ALL, "ru");
 
-	Printer<string> p;
-	p.Print("Hello, World!");
+	int* ptr = new int(5);
+	cout << *ptr << endl;
+	delete ptr;
 
+	SmartPointer<int> pointer = new int(10);
+	*pointer = 154857;
+	cout << *pointer << endl;
 
 	return 0;
 }
