5118b590df794ca98990bb3ced08197ffcd29c1c
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-02-25 22:39:14 +0300

    Специализация классов

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -9,125 +9,146 @@
 using namespace std;
 
 
-															/// ПРОМТРАНСТВО ИМЕН С++
+															/// СПЕЦИАЛИЗАЦИЯ ШАБЛОНОВ КЛАССА
+															
 								                         
 
-class Point
+//class Point
+//{
+//public:
+//	Point();
+//	Point(int x, int y, int z);
+//	~Point();
+//
+//private:
+//
+//	int x;
+//	int y;
+//	int z;
+//};
+//
+//
+//Point::Point()
+//{
+//	 x = y = z = 0;
+//}
+//
+//Point::Point(int x, int y, int z)
+//{
+//	this->x = x;
+//	this->y = y;
+//	this->z = z;
+//}
+//
+//Point::~Point()
+//{
+//}
+//
+//
+//template<typename/*или class*/ T1, typename T2>
+//class MyClass
+//{
+//public:
+//	MyClass(T1 value1, T2 value2);
+//	~MyClass();
+//
+//
+//	void DataTypeSize()
+//	{
+//		cout << sizeof(value1) << endl;
+//		cout << sizeof(value2) << endl;
+//	}
+//
+//
+//protected:
+//	T1 value1;
+//	T2 value2;
+//
+//	private:
+//
+//};
+//template <typename T1, typename T2>
+//MyClass<T1, T2>::MyClass(T1 value1, T2 value2)
+//{
+//	this->value1 = value1;
+//	this->value2 = value2;
+//}
+//template <typename T1, typename T2>
+//MyClass<T1, T2>::~MyClass()
+//{
+//}
+//
+//
+//template <typename T1, typename T2>
+//class TypeInfo : public MyClass<T1, T2>
+//{
+//public:
+//	TypeInfo(T1 value1, T2 value2) ;
+//	~TypeInfo();
+//
+//	void ShowTypeName()
+//	{
+//		cout << "Typename T1 is " << typeid(this->value1).name() << endl;
+//		cout << "Typename T2 is " << typeid(this->value2).name() << endl;
+//		
+//	}
+//
+//private:
+//
+//};
+//
+//template <typename T1, typename T2>
+//TypeInfo<T1, T2>::TypeInfo(T1 value1, T2 value2):MyClass<T1, T2>(value1, value2)
+//{
+//
+//}
+//
+//template <typename T1, typename T2>
+//TypeInfo<T1, T2>::~TypeInfo()
+//{
+//}
+template <typename T>
+class Printer
 {
 public:
-	Point();
-	Point(int x, int y, int z);
-	~Point();
+	Printer();
+	~Printer();
+	void Print(T value)
+	{
+		cout << value << endl;
+	}
 
 private:
 
-	int x;
-	int y;
-	int z;
 };
-
-
-Point::Point()
+template <typename T>
+Printer<T>::Printer()
 {
-	 x = y = z = 0;
 }
-
-Point::Point(int x, int y, int z)
-{
-	this->x = x;
-	this->y = y;
-	this->z = z;
-}
-
-Point::~Point()
+template <typename T>
+Printer<T>::~Printer()
 {
 }
 
-
-template<typename/*или class*/ T1, typename T2>
-class MyClass
+template<>
+class Printer<string>								/// Это и есть специализация. Ксли никакой реализации нет, то это запрет на string
 {
 public:
-	MyClass(T1 value1, T2 value2);
-	~MyClass();
-
-
-	void DataTypeSize()
+	void Print(string value)
 	{
-		cout << sizeof(value1) << endl;
-		cout << sizeof(value2) << endl;
+		cout << "___"<<value << "___"<<endl;
 	}
 
-
-protected:
-	T1 value1;
-	T2 value2;
-
-	private:
-
 };
-template <typename T1, typename T2>
-MyClass<T1, T2>::MyClass(T1 value1, T2 value2)
-{
-	this->value1 = value1;
-	this->value2 = value2;
-}
-template <typename T1, typename T2>
-MyClass<T1, T2>::~MyClass()
-{
-}
-
-
-template <typename T1, typename T2>
-class TypeInfo : public MyClass<T1, T2>
-{
-public:
-	TypeInfo(T1 value1, T2 value2) ;
-	~TypeInfo();
-
-	void ShowTypeName()
-	{
-		cout << "Typename T1 is " << typeid(this->value1).name() << endl;
-		cout << "Typename T2 is " << typeid(this->value2).name() << endl;
-		
-	}
-
-private:
-
-};
-
-template <typename T1, typename T2>
-TypeInfo<T1, T2>::TypeInfo(T1 value1, T2 value2):MyClass<T1, T2> (value1, value2)
-{
-
-}
-
-template <typename T1, typename T2>
-TypeInfo<T1, T2>::~TypeInfo()
-{
-}
-
-
-
 
 int  main()
 
 {
 	setlocale(LC_ALL, "ru");
 
-	double a = 2.2541;
+	Printer<string> p;
+	p.Print("Hello, World!");
 
-	Point b;
-
-	MyClass<double, Point> c(a, b);
-
-	TypeInfo<double, Point> d(a,b);
-	
-
-	c.DataTypeSize();
-	d.DataTypeSize();
-	d.ShowTypeName();
-	
 
 	return 0;
 }
