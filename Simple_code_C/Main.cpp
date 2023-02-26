e75f92b6e887f451e3f4155bd5ad200fc091fac8
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-02-26 21:54:12 +0300

    Односвязные списки Часть 1

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -10,37 +10,102 @@
 using namespace std;
 
 
-															/// Умные_указатели_Smart_pointers
-															/// auto_ptr, unique_ptr, shared_ptr
-template<class T>															
-class SmartPointer
+															/// ОДНОСВЯЗНЫЙ СПИСОК
+
+
+template<typename T>
+class List
 {
 public:
-	SmartPointer(T* ptr);
-	~SmartPointer();
+	List();
+	~List();
 
-	T& operator *()
+	void push_back(T data);
+	int GetSize() { return Size; }
+	T& operator[](const int index);
+
+	template<typename T>
+	class Node
 	{
-		return *ptr;
-	}
+	public:
+		Node(T data=T(), Node* pNext = nullptr)/// фактически параметры по умолчанию при объявлении, чтобы не было мусора, и для последнего члена списка
+		{
+			this->data = data;
+			this->pNext = pNext;
+		}  
+		~Node(){}
 
+		Node* pNext;
+		T data;
 
+		
+	private:
+		
+	};
 private:
-	T* ptr;
+	int Size;
+	Node<T>* head;
 };
 
-template<class T>
-SmartPointer<T>::SmartPointer(T*ptr)
+
+
+template<typename T>
+List<T>::List()
 {
-	this->ptr = ptr;
-	cout << "Constructor" << endl;
+	Size = 0;
+	head = nullptr;
 }
-template<class T>
-SmartPointer<T>::~SmartPointer()
+template<typename T>
+List<T>::~List()
 {
-	delete ptr;
-	cout << "Destructor" << endl;
 }
+
+template<typename T>
+void List<T>::push_back(T data)
+{
+	if (head == nullptr)
+		head = new Node<T>(data);
+	else
+	{
+		Node<T>* current = this->head;
+		while (current->pNext != nullptr)
+		{
+			current = current->pNext;
+		}
+		current->pNext = new Node<T>(data);
+	}
+	++Size;
+}
+
+template<typename T>
+T& List<T>::operator[](const int index)
+{
+	int counter = 0;
+	Node<T>* current = this->head;
+	while (current != nullptr)
+	{
+		if (counter == index)
+			return current->data;
+		else
+			current = current->pNext;
+		++counter;
+
+	}
+}
+
+
+
+//template<typename T>
+//List<T>::Node<T>::Node
+//{
+//	this->data = data;
+//	this->pNext = pNext;
+//}
+//
+//template<typename T>
+//List<T>::Node<T>::~Node()
+//{
+//}
 
 
 
@@ -48,54 +113,25 @@ int  main()
 
 {
 	setlocale(LC_ALL, "ru");
+	List<int> lst;
 
-	/*int* ptr = new int(5);
-	cout << *ptr << endl;
-	delete ptr;*/
+	int numbersCount;
+	cin >> numbersCount;
 
-	SmartPointer<int> pointer = new int(10);
-	*pointer = 154857;
-	cout << *pointer << endl;
-
-	//SmartPointer<int> sp = pointer;                 /// ПРОБЛЕММА
-
-	auto_ptr<int>ap1(new int(10));
-	auto_ptr<int> ap2(ap1);							/// Первый указатель затирается
-
-	
-	
-	
-	unique_ptr<int>up1(new int(10));
-	//unique_ptr<int> up2(up1);						/// запрет 
-	unique_ptr<int> up2 = move(up1);				/// перенос
-
-	/// ИЛИ
-
-	up2.swap(up1);									/// обмен
-
-	int* p = up1.get();								/// возврат сырого указателя
-
-	up1.release();									/// затирает указатель, но данные оставляет
-	
-	up1.reset();									/// затирает указатель и  данные
-
-
-	shared_ptr<int>sp1(new int(10));				/// общие указатели, данные уничтожаются только тогда когда уничтожается последнгий указатель
-	shared_ptr<int> sp2(sp1);						/// реализуется при помощи переменной static
-
-	
-													
-													/// МАССИВЫ
-
-	int SIZE ;
-	cin >> SIZE;
-	shared_ptr<int[]>ptr(new int[SIZE] {1, 23, 4, 56, 7});
-
-	for (int i = 0; i < SIZE; ++i)
+	for (int i = 0; i < numbersCount; ++i)
 	{
-		ptr[i] = rand() % 100;
-		cout << ptr[i] << endl;
+		lst.push_back(rand() % 100);
 	}
-	
+
+
+	/*lst.push_back(5);
+	lst.push_back(10);
+	lst.push_back(22);*/
+	cout<<lst.GetSize()<< endl;
+
+	cout << lst[0] << endl;
+
+	for (int i = 0; i < lst.GetSize(); ++i)
+		cout << lst[i] << endl;
 	return 0;
  }
