808039b199e453a5cd4cb9a6f2381ec56f53f12e
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-03-05 22:07:45 +0300

    контейнер LIST

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -8,10 +8,19 @@
 #include<Windows.h>							/// для изменения кодировки консоли
 #include<memory>							/// умные указатели
 #include<vector>
+#include<list>								/// подключаем LIST
 using namespace std;
 
 
-															/// Библиотека STL. VECTOR
+															/// Библиотека STL. LIST (Двухсвязанный список)
+
+
+template<class T>
+void PrintList(const list<T>& lst)
+{
+	for (auto it = lst.cbegin(); it != lst.cend(); ++it)
+		cout << *it << endl;
+}
 
 
 int  main()
@@ -19,109 +28,81 @@ int  main()
 {
 	setlocale(LC_ALL, "ru");
 	
-	vector<int> myVector;
-	myVector.push_back(2);
-	myVector.push_back(33);
-	myVector.push_back(25);
-	myVector.push_back(218);
-	myVector[0] = 1000;
+	list<int> myList;
 
-	//cout << myVector[10] << endl;								 /// может дать либо сбой, либо чужие данные
+	myList.push_back(5);										/// так же есть pop.back(), pop.front(), size, 
+	myList.push_front(10);
+	myList.push_back(520);
 
-	/*try
-	{
-		cout << myVector.at(10) << endl;                          /// метод at  в любом случае не дает возможность доступа к чужим данным, но он медленный
-	}
-	catch (const std::out_of_range & ex)
-	{
-		cout << ex.what() << endl << endl;
-	}*/
+	//myList[0]   - оператор не перегружен
 
-	for (int i = 0; i < myVector.size(); ++i)
-		cout << myVector[i] << endl;							/// метод size  - размер
+	auto it = myList.begin();
 
-
-	//myVector.clear();											/// очищает vector
-
-	cout << myVector.size() << endl << endl;
 	
-	myVector.pop_back();
+	for(auto it = myList.begin();it != myList.end(); ++it)
+		cout << *it << endl;
 
-	for (int i = 0; i < myVector.size(); ++i)
-		cout << myVector[i] << endl;
+	list<int> myList1{ 1,2,3,4,5,6 };      /// можно и так инициализировать
 
-	cout << myVector.size() << endl << endl;
+	for (auto it = myList1.begin(); it != myList1.end(); ++it)
+		cout << *it << endl;
 
-	vector<int>  myVector2 = { 1,2,3,4,5 };
-	myVector2.push_back(10);
-	myVector2.push_back(30);
-	myVector2.push_back(40);
-	myVector2.push_back(100);
+	PrintList(myList);
 
+	myList.sort();
+	PrintList(myList);
+	cout << endl << endl;
 
-	/*for (int i = 0; i < myVector2.size(); ++i)
-		cout << myVector2[i] << endl;*/
+	list<int> myList2{ 1,2,3,5,2,1,4,8,1,1,1,2,9,6,3,3,2 };
+	PrintList(myList2);
 
-	//myVector.capacity();
-	cout << myVector2.capacity()  << endl;                   /// это в принципе количество ячеек - при добавлении элементов иногда заранее 
-															/// выделяется массив с дополнительной ячейкой - сколько их определяется индивидуально 
-															/// в зависимости от размера массива и пр.
-	myVector2.reserve(100);									/// это можно самому регулировать
-															
-	cout << myVector2.capacity() << endl;
-
-	myVector2.shrink_to_fit();
-	cout << myVector2.capacity() << endl;					///оптимизирует размер
-	
-	vector<int> myVector3(20);								/// установление размера при инициализации
-	for (int i = 0; i < myVector3.size(); ++i)				/// при таком написании инициализируются нулями
-		cout << myVector3[i] << endl;	
-	vector<int> myVector4(20,100);							/// при таком написании инициализируются второй цифрой в скобках
-	for (int i = 0; i < myVector4.size(); ++i)				
-		cout << myVector4[i] << endl;
-
-	cout<<myVector4.empty()<<endl<<endl<<endl;				/// bool - пустой ли массивж
-
-	myVector4.resize(10);									/// меняет размер массива, если увеличивает то дописывает нулями
-	for (int i = 0; i < myVector4.size(); ++i)
-		cout << myVector4[i] << endl;
-	cout << endl;
-	myVector4.resize(20,200);								/// меняет размер массива, если увеличивает то дописывает вторым параметром в скобках		
-	for (int i = 0; i < myVector4.size(); ++i)
-		cout << myVector4[i] << endl;
+	cout << endl << endl;
+	myList2.unique();								/// удаляет дубликаты которые идут подряд
+	PrintList(myList2);
 
 	cout << endl << endl;
 
-	///ИТЕРАТОРЫ ДЛЯ СЛЕДУЮЩИХ МЕТОДОВ
+	myList2.reverse();
 	
-	vector<int>::iterator i = myVector.begin();
+	PrintList(myList2);							/// размещает лист в обратном порядке
 
-	advance(i, 2);    ///или i+=2
+	cout << endl << endl;
+	myList1.clear();
+	PrintList(myList1);
 
-	myVector.insert(i, 999);
+	cout << endl << endl;
+
+	auto i = myList2.begin();					/// вставить новый элемент
+	advance(i, 3);
+	myList2.insert(i, 100);
+	PrintList(myList2);
+
+	cout << endl << endl;
+
+	myList2.erase(i);                 /// так как это не индекс, а указатель, поэтому итераторы лучше для каждого метода делать свои
+	myList2.push_back(100);
+	PrintList(myList2);
+
 	
-	for (vector<int>::iterator i = myVector.begin(); i < myVector.end(); ++i)
-		cout << *i << endl;
+
+	cout << endl << endl;
+
+	myList2.remove(100);           /// удаляет ВЕЗДЕ конкретное число если найдет
+	PrintList(myList2);
+
+	cout << endl << endl;
+
+	myList2.assign(5, 0);           ///  удаляет ВСЕ и заполняет лист - первый поараметр - количество элементов, второй - число, которым заполняется лист
+	PrintList(myList2);
+
+	cout << endl << endl;
+
+	myList2.assign(myList.begin(), myList.end());           ///  переписывает один список в другой
+	PrintList(myList2);
 	
-
-	vector<int>::iterator i1 = myVector.begin();
-	myVector.erase(i1);
-
-
-	for (vector<int>::iterator i = myVector.begin(); i < myVector.end(); ++i)
-		cout << *i << endl;
-
-	///УДАЛЕНИЕ ДИАПАЗОНА ЭЛЕМЕНТОВ
-
-	vector<int>::iterator i2= myVector4.begin();
-	myVector4.erase(i2,	i2+5);
-
-
-	for (vector<int>::iterator i = myVector4.begin(); i < myVector4.end(); ++i)
-		cout << *i<< endl;
-
-
-
+	
+	
+	
 	return 0;
 
 
