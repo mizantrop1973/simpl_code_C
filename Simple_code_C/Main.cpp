23a60b1af51a00f42654d9a61d4551009b5ae748
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-03-08 04:12:47 +0300

    set, multiset - ассоциативные контейнеры

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -7,37 +7,97 @@
 #include<string>
 #include<Windows.h>							/// для изменения кодировки консоли
 #include<memory>							/// умные указатели
-#include<vector>
-#include<list>								/// подключаем LIST
-#include<forward_list>	
-#include<array>			
-#include<deque>								/// двухсторонняя очередь
+
+#include<set>								/// 
 using namespace std;
 
 
-															///Контейнер deque двухсторонняя очередь - гибрид между вектором и двухсторонним списком
-															/// по сути двухсвязанный список векторов
-
-
-template<class T>
-void PrintList(const list<T>& lst)
-{
-	for (auto it = lst.cbegin(); it != lst.cend(); ++it)
-		cout << *it << endl;
-}
-
+															/// Ассоциативный контейнер SET, multiset
+															/// Упорядочные элементы - БИНАРНОЕ ДЕРЕВО 
+															/// Только уникальные элементы
 
 int  main()
 
 {
 	setlocale(LC_ALL, "ru");
 	
-	deque<int> dq{ 1,2,3,4,5 };
+	set<int> mySet{2,6,9,7,1};       /// Бинарное деоево с уникальными значениями , в порядке возрастания
 
-	dq.begin();
-	
+	mySet.insert(5);
+	mySet.insert(1);
+	mySet.insert(12);
+
+	mySet.insert(4);
+	mySet.insert(-1);
+
+	mySet.insert(5);
+
+	for (auto& item : mySet)
+		cout << item << endl;
+
+	cout << endl << endl;
+
+	for (int i = 0; i < 20; ++i)
+		mySet.insert(rand() % 10);
+
+	for (auto& item : mySet)
+		cout << item << endl;
+
+	cout << endl << endl;
+
+	/// поиск элементов , удаление и вставка - чтобы изхменить элемент в set
+
+	set<int> mySet1{ 12,34,56,2,-1,99,37 };
+
+	auto it = mySet1.find(10);      /// возвращает итератор на той позиции где это число, или на следующий после последнего "элемент" (где он мог бы быть)
+
+	auto it1 = mySet1.find(2);
+
+	int value = 56;
+
+	if (mySet1.find(value) != mySet1.end())
+		cout << "Number " << value << " was found";
+	else
+		cout << "Number " << value << " wasn't found";
+
+	cout << endl << endl;
+
+	mySet1.erase(199);
+	mySet1.insert(77);                  /// возвращается 2 значения, 1 - итератор, 2 -bool (1 если добавили, 0, если такое число уже было)
+
+	for (auto& item : mySet1)
+		cout << item << endl;
+
+	cout << endl << endl;
+
+	multiset<int> ms{ 2,3,4,6,8,0,3,3,4,4,6 };     /// может содержать дубли
+
+	for (auto& item : ms)
+		cout << item << endl;
+
+	cout << endl << endl;
+
+	ms.find(3);
+	ms.lower_bound(4);								/// возвращает первую найденную четверку (итератор, то есть указатель)
+	 
+	auto s = ms.upper_bound(4);								/// вернет нам следующий элемент после 4 по возрастанию.
+
+	ms.erase(3);
+	ms.insert(4);
+
+	for (auto& item : ms)
+		cout << item << endl;
+
+	cout << endl << endl;
+
+	cout << *s << endl;
+
+	cout << endl << endl;
+
+	auto d = ms.equal_range(4);
 
 
+	//cout << *d << endl;
 	return 0;
 
 
