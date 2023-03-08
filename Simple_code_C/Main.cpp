fc6178a2a5ef829265ba97955e038572bc5b3087
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-03-08 04:42:12 +0300

    typedef

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -1,63 +1,26 @@
-#include<iostream>
+include<iostream>
 #include<ctime>
 #include <math.h>
 #include<algorithm>
 #include<locale.h>
 #include<vector>;
-#include<list>
+
+
+
 using namespace std;
+typedef vector<int> int_vector;							/// ВОТ ТУТ ЕГО ОПРЕДЕЛЯЕМ, НО МОЖНО И В main
 
-
-														/// ЦИКЛ for... each  - не стандарт!!!! И не поддерживается
-														/// есть алгоритм for_each  - не путать (algoritm)
-														/// остался так называемый range-based цикл (цикла для диапазона) - ниже в коде. СТАНДАРТ!
-
-
-
-template<typename T>
-void PrintList(const list<T>& lst)
-{
-	for (auto i = lst.cbegin(); i != lst.cend(); ++i)							/// укоротили и упростили код 
-		cout << *i << endl;
-}
-
+												  /// КЛЮЧЕВОЕ СЛОВО typedef
 
 int main()
 
 {
 	setlocale(LC_ALL, "ru");
 
-	int arr[]={ 1,2,3,4,5 };
+	/*std::*/vector<int> v;
 
-	for(auto var: arr)									/// передается копия, если мы что то меняем, то массив не меняется
-	{
-		var = -1;
-		cout << var << endl;
-	}
+	int_vector iv;
 
-	for (auto &var : arr)									/// передается ссылка, массив меняется
-	{
-		var = 100;
-		cout << var << endl;
-	}
-	
-
-
-	for (const auto& var : arr)									/// передается ссылка, массив меняется
-	{
-		///var = 100;											/// ошибка
-		cout << var << endl;
-	}
-	
-	list<int> myList{ 9,8,7,6,5 };
-	
-	PrintList(myList);
-	
 	return 0;
-
-
-
-
-
 
 }
