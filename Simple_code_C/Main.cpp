e970cbcf1ea21d11217b59d699ef8cd9f1144d7b
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-03-10 20:16:42 +0300

    Возврат результата из потока

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -10,22 +10,24 @@
 using namespace std;
 
 
-															/// ИНОГОПОТОЧНОЕ ПРОГРАММИРОВАНИЕ, ПЕРЕДАЧА ПАРАМЕТРОВ В ПОТОК
+															/// ИНОГОПОТОЧНОЕ ПРОГРАММИРОВАНИЕ, ВОЗВРАТ РЕЗУЛЬТАТА ИЗ ПОТОКА ПО ССЫЛКЕ
 
 
 
 
 
 
-void DoWork(int a, int b, string msg)														
+void Sum(int a, int b, int &p)													
 {
-	cout << msg << endl;
+	
 	this_thread::sleep_for(chrono::milliseconds(3000));
-	cout << "============================\tDoWorked STARTED\t========================================" << endl;
+	cout <<"ID stream =  "<< this_thread::get_id()<< "============================\tSum STARTED\t========================================" << endl;
 	this_thread::sleep_for(chrono::milliseconds(5000));
-	cout << "a  +  b  =  " << a + b << endl;
+	p = a + b;
 	this_thread::sleep_for(chrono::milliseconds(3000));
-	cout << "============================\tDoWorked STOPED\t=========================================" << endl;
+	cout << "ID stream =  " << this_thread::get_id() << "============================\tSum STOPED\t=========================================" << endl;
+
+	
 }
 
 
@@ -34,15 +36,20 @@ int  main()
 {
 	setlocale(LC_ALL, "ru");
 
-	thread th(DoWork, 2,3, "Sum");              /// передача параметров в поток - обязательно соблюдать количество и типа параметров
+	int p = 0;
 
-	for (size_t i = 0;true; i++)     /// постоянная работа
+	///thread th (Sum,2,3, p);              /// передача параметров в поток - обязательно соблюдать количество и типа параметров
+
+	thread th(Sum, 2, 3, std::ref(p));    ///вот как надо !!!!!!!!!!!!!!!!!!!!!
+
+	for (size_t i = 0;i<10; i++)     /// постоянная работа
 	{
 		cout << "Stream's ID = " << this_thread::get_id() << "\tmain works\t" << i << endl;
 		this_thread::sleep_for(chrono::milliseconds(500));
 	}
 
 	th.join();
+	cout << "a  +  b  =  " << p << endl;     /// будет ошибка, так как в случае с другим потоком параметр передается в функцию все равно по значению
 
 	return 0;
  }
