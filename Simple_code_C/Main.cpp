8d26ecd398adcf212ade137da93b49c32e85401f
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-03-16 16:56:06 +0300

    Паттерн Фабрика

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -9,49 +9,327 @@
 #include<chrono>							/// для времени
 using namespace std;
 
+//
+//															/// ЛЯМБДА ВЫРАЖЕНИЯ И ВОЗВРАТ РЕЗУЛЬТАТА ВЫПОЛНЕНИЯ ПОТОКА
+//
+//
+//
+//
+//
+//
+//int  Sum(int a, int b)													
+//{
+//	
+//	this_thread::sleep_for(chrono::milliseconds(3000));
+//	cout <<"ID stream =  "<< this_thread::get_id()<< "============================\tSum STARTED\t========================================" << endl;
+//	this_thread::sleep_for(chrono::milliseconds(5000));
+//
+//	this_thread::sleep_for(chrono::milliseconds(3000));
+//	cout << "ID stream =  " << this_thread::get_id() << "============================\tSum STOPED\t=========================================" << endl;
+//
+//	return a + b;
+//}
+//
+//
+//int  main()
+//
+//{
+//	setlocale(LC_ALL, "ru");
+//
+//	int result;
+//	thread t (Sum, 2, 5);
+//
+//	//cout << "Sum result" << result << endl;
+//
+//	for (size_t i = 0;i<10; i++)     
+//	{
+//		cout << "Stream's ID = " << this_thread::get_id() << "\tmain works\t" << i << endl;
+//		this_thread::sleep_for(chrono::milliseconds(500));
+//	}
+//
+//	t.join();
+//
+//
+//	return 0;
+// }
+//
 
-															/// ИНОГОПОТОЧНОЕ ПРОГРАММИРОВАНИЕ, ВОЗВРАТ РЕЗУЛЬТАТА ИЗ ПОТОКА ПО ССЫЛКЕ
+//class Geometry
+//{
+//public:
+//	Geometry();
+//	~Geometry();
+//	virtual void visit (GetArea &viss) = 0;
+//
+//private:
+//
+//};
+//
+//Geometry::Geometry()
+//{
+//}
+//
+//Geometry::~Geometry()
+//{
+//}
+//
+//
+//class Triangle: public Geometry
+//{
+//public:
+//	Triangle(int a, int b, int c);
+//	~Triangle();
+//	void visit(GetArea& viss)
+//	{
+//		cout << a + b + c << endl;
+//	}
+//private:
+//	int a;
+//	int b;
+//	int c;
+//};
+//
+//Triangle::Triangle(int a, int b, int c) : a(a), b(b), c(c)
+//{
+//
+//}
+//
+//Triangle::~Triangle()
+//{
+//}
+//
+//class Square:public Geometry
+//{
+//public:
+//	Square(int a, int b);
+//	~Square();
+//	void visit(GetArea& viss)
+//	{
+//		cout << a + b + a + b << endl;
+//	}
+//
+//private:
+//	int a;
+//	int b;
+//
+//};
+//
+//Square::Square(int a, int b) : a(a), b(b)
+//{
+//}
+//
+//Square::~Square()
+//{
+//}
+//
+//class Circle:public Geometry
+//{
+//public:
+//	Circle(int r);
+//	~Circle();
+//	void visit(GetArea& viss)
+//	{
+//		cout << 2*r*3<< endl;
+//	}
+//private:
+//	int r;
+//};
+//
+//Circle::Circle(int r) : r(r)
+//{
+//}
+//
+//Circle::~Circle()
+//{
+//}
+//
+//
+//class GetArea 
+//{
+//public:
+//	GetArea();
+//	~GetArea();
+//	int getLength(int a, int b, int c)
+//	{
+//		cout << a + b + c << endl;
+//	}
+//	int getLength(int a, int b)
+//	{
+//		cout << 2*(a + b) << endl;
+//	}
+//	int getLength(int r)
+//	{
+//		cout << 6*r << endl;
+//	}
+//
+//
+//private:
+//
+//};
+//
+//GetArea::GetArea()
+//{
+//}
+//
+//GetArea::~GetArea()
+//{
+//}
+//
+//int main()
+//{
+//	Geometry* geo;
+//	Triangle* tri = new Triangle(1, 1, 1);
+//	Square* sq = new Square(2, 2);
+//	Circle* cq = new Circle(3);
+//	geo = tri;
+//	GetArea viss;
+//	geo->visit(&viss);
+//	geo = sq;
+//	geo->visit(&viss);
+//	geo = cq;
+//	geo->visit(&viss);
+//	
+//	
+//	
+//	
+//	return 0;
+//}
 
-
-
-
-
-
-void Sum(int a, int b, int &p)													
+class IProduction
 {
-	
-	this_thread::sleep_for(chrono::milliseconds(3000));
-	cout <<"ID stream =  "<< this_thread::get_id()<< "============================\tSum STARTED\t========================================" << endl;
-	this_thread::sleep_for(chrono::milliseconds(5000));
-	p = a + b;
-	this_thread::sleep_for(chrono::milliseconds(3000));
-	cout << "ID stream =  " << this_thread::get_id() << "============================\tSum STOPED\t=========================================" << endl;
+public:
+	IProduction();
+	~IProduction();
+	virtual void release() = 0;
 
-	
+private:
+
+};
+
+IProduction::IProduction()
+{
 }
 
-
-int  main()
-
+IProduction::~IProduction()
 {
-	setlocale(LC_ALL, "ru");
+}
 
-	int p = 0;
-
-	///thread th (Sum,2,3, p);              /// передача параметров в поток - обязательно соблюдать количество и типа параметров
-
-	thread th(Sum, 2, 3, std::ref(p));    ///вот как надо !!!!!!!!!!!!!!!!!!!!!
-
-	for (size_t i = 0;i<10; i++)     /// постоянная работа
+class Car : public IProduction
+{
+public:
+	Car();
+	~Car();
+	void release() override
 	{
-		cout << "Stream's ID = " << this_thread::get_id() << "\tmain works\t" << i << endl;
-		this_thread::sleep_for(chrono::milliseconds(500));
+		cout << "A new auto have been constructed" << endl;
 	}
 
-	th.join();
-	cout << "a  +  b  =  " << p << endl;     /// будет ошибка, так как в случае с другим потоком параметр передается в функцию все равно по значению
+private:
 
+};
+
+Car::Car()
+{
+}
+
+Car::~Car()
+{
+}
+
+class Truck : public IProduction
+{
+public:
+	Truck();
+	~Truck();
+	void release() override
+	{
+		cout << "A new truck have been constructed" << endl;
+	}
+private:
+
+};
+
+Truck::Truck()
+{
+}
+
+Truck::~Truck()
+{
+}
+
+class IworkShop
+
+{
+public:
+	IworkShop();
+	~IworkShop();
+	virtual IProduction* create() = 0;
+
+private:
+
+};
+
+IworkShop::IworkShop()
+{
+}
+
+IworkShop::~IworkShop()
+{
+}
+
+class CarWorkShop:public IworkShop
+{
+public:
+	CarWorkShop();
+	~CarWorkShop();
+	IProduction* create() override
+	{
+		return new Car();
+	}
+
+private:
+
+};
+
+CarWorkShop::CarWorkShop()
+{
+}
+
+CarWorkShop::~CarWorkShop()
+{
+}
+
+class TruckWorkShop:public IworkShop
+{
+public:
+	TruckWorkShop();
+	~TruckWorkShop();
+	IProduction* create() override
+	{
+		return new Truck();
+	}
+
+private:
+
+};
+
+TruckWorkShop::TruckWorkShop()
+{
+}
+
+TruckWorkShop::~TruckWorkShop()
+{
+}
+int main()
+{
+
+	IworkShop* creator = new CarWorkShop();
+	IProduction* car = creator->create();
+
+	creator = new TruckWorkShop();
+	IProduction* truck = creator->create();
+
+	car->release();
+	truck->release();
 	return 0;
- }
-
-
+}
