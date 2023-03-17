f1cb1d403d856c28784ad78bc0c7fd29203ca484
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2023-03-17 19:10:03 +0300

    Организация таймеров выполнения кода

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -6,15 +6,43 @@
 #include<fstream>							/// поток в файлы
 #include<string>
 #include<thread>							/// дл€ потоков
-#include<chrono>							/// дл€ времени
+//#include<chrono>							/// дл€ времени (при включенной библиотеке потоков подключаетс€ автоматом)
 using namespace std;
+class Timer															/// сздаем класс измер€ющий врем€ работы программы
+{
+public:
+	Timer();
+	~Timer();
 
 
-															/// ЋяћЅƒј. «ј’¬ј“  ќЌ“≈ —“ј
+private:
+	std::chrono::time_point<std::chrono::steady_clock> start, finish;
+};
 
+Timer::Timer()
+{
+	start = chrono::high_resolution_clock::now();
+}
 
+Timer::~Timer()
+{
+	finish = chrono::high_resolution_clock::now();
+	chrono::duration<float> duration = finish - start;
+	cout << duration.count() << " sec" << endl;
+}
 
+															/// ¬–≈ћя ¬џѕќЋЌ≈Ќ»я  ќƒј
 
+int Sum(int a, int b)
+
+{
+	Timer n;
+	this_thread::sleep_for(chrono::milliseconds(3000));
+	cout << "Stream ID  is " << this_thread::get_id() << " ==============================\t SUM STARTED\t===============================" << endl;
+	this_thread::sleep_for(chrono::milliseconds(3000));
+	cout << "Stream ID  is " << this_thread::get_id() << " ==============================\t SUM STOPED\t===============================" << endl;
+	return a + b;
+}
 
 
 
@@ -22,17 +50,32 @@ using namespace std;
 int  main()
 
 {
+	Timer a;													/// нужно всего лишь создать вначале программы класс, куда вынесен расчет времени 
+	
 	setlocale(LC_ALL, "ru");
 
-	int a = 55;
+	int result;
 
-	auto f = [a]()
+	thread t([&result]()
+		{
+			result = Sum(2, 5);
+		}
+	);
+
+
+	for (size_t i = 1; i <= 10; ++i)
 	{
-		cout << a << endl;
-	};
+		cout << "Stream ID  is " << this_thread::get_id() << " ==============================\t MAIN STARTED\t===============================" <<i<< endl;
+		this_thread::sleep_for(chrono::milliseconds(500));
+	}
 	
-	f();
 
+	t.join();
+
+	cout << "Sum result is " << result << endl;
+
+	
+	
 	return 0;
  }
 
