e79dcbdf32b89c8fbd5800a6dae36856a2cd0878
Mizantrop <sds1973@mail.ru>
2023-03-22 21:25:37 +0300

    МНОГОПОТОЧНОЕ ПРОГРАММИРОВАНИЕ, МЕТОД КЛАССА В ПОТОКЕ

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -8,75 +8,103 @@
 #include<thread>							/// для потоков
 //#include<chrono>							/// для времени (при включенной библиотеке потоков подключается автоматом)
 using namespace std;
-class Timer															/// сздаем класс измеряющий время работы программы
+										/// ИНОГОПОТОЧНОЕ ПРОГРАММИРОВАНИЕ, МЕТОД КЛАССА В ПОТОКЕ
+
+
+
+
+
+
+
+class MyClass
 {
 public:
-	Timer();
-	~Timer();
+	MyClass();
+	~MyClass();
+	void DoWork()
+	{
+		this_thread::sleep_for(chrono::milliseconds(3000));
+		cout << "ID stream =  " << this_thread::get_id() << "============================\tDoWork STARTED\t========================================" << endl;
+		this_thread::sleep_for(chrono::milliseconds(5000));
+		cout << "ID stream =  " << this_thread::get_id() << "============================\tDoWork STOPED\t=========================================" << endl;
+	}
 
+
+	void DoWork2(int a)
+	{
+		this_thread::sleep_for(chrono::milliseconds(3000));
+		cout << "ID stream =  " << this_thread::get_id() << "=============================\tDoWork2 STARTED\t========================================" << endl;
+		this_thread::sleep_for(chrono::milliseconds(5000));
+		cout << "DoWork2 value of the parameter\t" << a << endl;
+		cout << "ID stream =  " << this_thread::get_id() << "=============================\tDoWork2 STOPED\t=========================================" << endl;
+	}
+
+
+	int Sum(int a, int b)
+	{
+
+		this_thread::sleep_for(chrono::milliseconds(3000));
+		cout << "ID stream =  " << this_thread::get_id() << "==============================\tSum STARTED\t========================================" << endl;
+		this_thread::sleep_for(chrono::milliseconds(5000));
+		this_thread::sleep_for(chrono::milliseconds(3000));
+		cout << "ID stream =  " << this_thread::get_id() << "==============================\tSum STOPED\t=========================================" << endl;
+		return a + b;
+
+	}
 
 private:
-	std::chrono::time_point<std::chrono::steady_clock> start, finish;
+
 };
 
-Timer::Timer()
+MyClass::MyClass()
 {
-	start = chrono::high_resolution_clock::now();
 }
 
-Timer::~Timer()
+MyClass::~MyClass()
 {
-	finish = chrono::high_resolution_clock::now();
-	chrono::duration<float> duration = finish - start;
-	cout << duration.count() << " sec" << endl;
 }
-
-															/// ВРЕМЯ ВЫПОЛНЕНИЯ КОДА
-
-int Sum(int a, int b)
-
-{
-	Timer n;
-	this_thread::sleep_for(chrono::milliseconds(3000));
-	cout << "Stream ID  is " << this_thread::get_id() << " ==============================\t SUM STARTED\t===============================" << endl;
-	this_thread::sleep_for(chrono::milliseconds(3000));
-	cout << "Stream ID  is " << this_thread::get_id() << " ==============================\t SUM STOPED\t===============================" << endl;
-	return a + b;
-}
-
-
 
 
 int  main()
 
 {
-	Timer a;													/// нужно всего лишь создать вначале программы класс, куда вынесен расчет времени 
-	
 	setlocale(LC_ALL, "ru");
+
+
+	MyClass m;
 
 	int result;
 
-	thread t([&result]()
+	thread t([&]()
 		{
-			result = Sum(2, 5);
-		}
-	);
+			result = m.Sum(2, 5);
+		});
 
+	thread t1(&MyClass::DoWork, m);						/// синтаксис подходит если не нужно возвращать значение
+
+
+	thread t2(&MyClass::DoWork2, m, 5);					/// если нужно передавать значение
 
 	for (size_t i = 1; i <= 10; ++i)
 	{
-		cout << "Stream ID  is " << this_thread::get_id() << " ==============================\t MAIN STARTED\t===============================" <<i<< endl;
-		this_thread::sleep_for(chrono::milliseconds(500));
+		cout << "ID stream =  " << this_thread::get_id() << "==============================\tmain works\t" << i << "========================================" << endl;
+		this_thread::sleep_for(chrono::milliseconds(3000));
 	}
-	
 
+
+	cout << "Result\t" << result << endl;
+
+	/*thread t1([&]()									/// вариант через лямбду
+		{
+			m.DoWork();
+		}); */
+
+
+
+
+	t2.join();
+	t1.join();
 	t.join();
 
-	cout << "Sum result is " << result << endl;
-
-	
-	
 	return 0;
- }
-
-
+}
