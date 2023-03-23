7c0e2f9e14d8b06a2b143a2cf8974a0bbb6fa696
Mizantrop <sds1973@mail.ru>
2023-03-23 03:03:45 +0300

    Equal  с бинарным предикатом (если не перегружено оператор сравнения

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -13,40 +13,28 @@
 using namespace std;
 
 
-														/// АЛГОРИТМЫ STL. equal and mismatch
+/// АЛГОРИТМЫ STL. equal с предикатом
 
-class Person
+class Point
 {
 public:
-	Person(string name, int age, double score);
-	~Person();
+	Point(int x, int y);
+	~Point();
 
-	bool operator()(const Person& p)
-	{
-		return p.score > 180;
-	}
 
-	string name;
-	double score;
-	int age;
-
+	int x, y;
 private:
-
 };
 
-Person::Person(string name, int age, double score)
+Point::Point(int x, int y)
 {
-	this->name = name;
-	this->age = age;
-	this->score = score;
-
+	this->x = x;
+	this->y = y;
 }
 
-Person::~Person()
+Point::~Point()
 {
 }
-
-
 
 int  main()
 
@@ -55,53 +43,19 @@ int  main()
 	setlocale(LC_ALL, "ru");
 
 
-	
+	vector<Point> arr1{ Point(20,3),Point(4,8),Point(0,10) };
 
-	int arr1[]{ 1,4,7,9,34,-77,45,42,11,29,10, 12};
-	int arr2[]{ 1,4,7,9,34,-77,45,42,11,29 ,10,11};      
+	vector<Point> arr2{ Point(1,3),Point(4,8),Point(0,10) };
 
-	bool result = equal(begin(arr1), end(arr1), begin(arr2));    /// будет некорректно работать если вторая коллекция будет длиннееб
-																/// но в пределах длинны первой коллекции данные будуь совпадать
+	bool result = equal(begin(arr1), end(arr1), begin(arr2), end(arr2), [](const Point & p1, const Point & p2) 
+		{
+			return  p1.x == p2.x && p1.y == p2.y;
+		});
 
-	bool result1 = equal(begin(arr1), end(arr1), begin(arr2), end(arr2));  /// не знаю зачем лектор говорил вообще о первом варианте!!!!!!!!
 
 	cout << result << endl;
-	cout << result1 << endl;
 
-	vector<int> v1{ 1,4,7,9,34,-77,45,42,11,29,1 };    /// можно сравнивать вектор и массив легко
-
-	bool result2 = equal(begin(arr1), end(arr1), begin(v1), end(v1));  
-
-
-	cout << result2 << endl;                      /// при сравнении equal важен порядок. Если есть сомнения в порядке - надо сортировать
-
-	vector<int> v2{ 1,4,7,9,34,-77,45,42,11,1,29 }; /// отличается от первого порядком последних членов например
-
-	sort(begin(v1), end(v1));     /// после сортировки будут равны
-
-	sort(begin(v2), end(v2));
-
-	for (auto element : v2)
-		cout << element << endl;
-
-	bool result3 = equal(begin(v1), end(v1), begin(v2), end(v2));
-	cout << result3 << endl;
-
-	auto result4 = mismatch(begin(arr1), end(arr1), begin(arr2), end(arr2));   /// возвращает итераторы на несовпадающие элементы
-
-	
-	if (result4.first == end(arr1) && result4.second == end(arr2))
-		cout << "Collections are equal" << endl;
-	else
-		cout << "Collections are not equal" << endl;
-	
-	
-	
-	cout << *result4.first << "\t" << *result4.second << endl;
-
-
-
-		return 0;
+	return 0;
 }
 
 
