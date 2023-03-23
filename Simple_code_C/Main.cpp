a85481f6ee7b901922596c5b3464c5ffc549cec4
Mizantrop <sds1973@mail.ru>
2023-03-23 18:28:02 +0300

    unoque, unique copy

--- Simple_code_C/Main.cpp
+++ Simple_code_C/Main.cpp
@@ -13,14 +13,9 @@
 using namespace std;
 
 
-													/// АЛГОРИТМЫ STL. for///each  - перебрать элементы коллекции
+													/// АЛГОРИТМЫ STL. unique - Удалить повторяющиеся элементы
 
 
-
-void Foo(int a)
-{
-	cout << a << endl;
-}
 
 
 int  main()
@@ -28,27 +23,36 @@ int  main()
 {
 	setlocale(LC_ALL, "ru");
 
+	/*int arr[]{ 1,2,3,3,3,5,9,7,4,1,1,5};
 
-	int arr[]{ 1,4,8,2,5,9,0 };
+	auto result = unique(begin(arr), end(arr));
 
-	for_each(begin(arr), end(arr), Foo);
-	
-	cout << endl << endl << endl;
-
-	for (auto element : arr)
-	{
-		cout << element << endl;
-	}
-
-	cout << endl << endl << endl;
-
-	for_each(begin(arr), end(arr), [](int a) 
+	for_each(begin(arr), result, [](int a)
 		{
 			cout << a << endl;
+
 		});
 
+	cout << endl << endl;*/
 
 
+
+	vector<int> v{ 2,3,4,4,4,6,7,7,7,86,55,55,44,3,3,0 };
+
+	/*auto result1 = unique(begin(v), end(v));
+
+	v.erase(result1, end(v));
+
+	for_each(begin(v), end(v), [](int a)
+		{
+			cout << a << endl;
+
+		});*/
+
+
+	vector<int> v1;
+
+	unique_copy(begin(v), end(v), back_inserter(v1));
 	return 0;
 }
 
